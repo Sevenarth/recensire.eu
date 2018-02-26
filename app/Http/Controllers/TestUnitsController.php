08fce4f5734dce1654fce8d3c6fe038c01392ad9
Luca Vizzarro <me@sowia.me>
2018-02-26 18:25:37 +0000

    new stuff

--- app/Http/Controllers/TestUnitsController.php
+++ app/Http/Controllers/TestUnitsController.php
@@ -10,9 +10,21 @@ use \Carbon\Carbon;
 class TestUnitsController extends Controller
 {
     public function view(Request $request, $testUnit) {
-      $testUnit = TestUnit::where('hash_code', $testUnit)->firstOrFail();
+      $testUnit = TestUnit::where('hash_code', $testUnit)->where('status', 0)->firstOrFail();
       if((new Carbon($testUnit->expires_on, config('app.timezone'))) < Carbon::now())
         abort(404);
       return view('test', compact('testUnit'));
+    }
+    public function go(Request $request, $testUnit) {
+      $testUnit = TestUnit::where('hash_code', $testUnit)->where('status', 0)->firstOrFail();
+      if((new Carbon($testUnit->expires_on, config('app.timezone'))) < Carbon::now())
+        abort(404);
+
+      if(!empty($testUnit->viewed)) {
+        $testUnit->viewed = true;
+        $testUnit->save();
+      }
+
+      return redirect($testUnit->reference_url);
     }
 }
---
+++ resources/views/thankyou.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.public')
+
+@section('title') Invito di test accettato @endsection
+
+@section('content')
+  <div class="container">
+    <div class="p-2 h5">
+      <b>Grazie mille!</b><br>
+      Grazie mille per avere accettato l'invito a partecipare a questa unità di test di un prodotto a noi affiliato. Verrai ricontattato dal nostro staff presto!
+    </div>
