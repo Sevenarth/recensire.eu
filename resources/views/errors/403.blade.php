7f78c22c9a8007a75d8d225061e3f79f9270a37d
Luca Vizzarro <me@sowia.me>
2018-02-27 20:57:06 +0000

    some fun added

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name') }} - Pagina riservata</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <style>html,body{background-color:#fff;color:#636b6f;font-family:'Raleway',sans-serif;font-weight:100;height:100vh;margin:0}.full-height{height:100vh}.flex-center{align-items:center;display:flex;justify-content:center}.position-ref{position:relative}.content{text-align:center;margin:10px}.title{font-size:36px;padding:20px}img#logo{max-height:150px}
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <img src="/images/logo.svg" alt="Logo" id="logo">
+                <div class="title">
+                  Heyy! Questa pagina è riservata! 😠
+                </div>
+                Purtroppo l'accesso a questa pagina non ti è concesso con i tuoi attuali permessi.
+            </div>
+        </div>
+    </body>
+</html>
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name') }} - Pagina non trovata</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <style>html,body{background-color:#fff;color:#636b6f;font-family:'Raleway',sans-serif;font-weight:100;height:100vh;margin:0}.full-height{height:100vh}.flex-center{align-items:center;display:flex;justify-content:center}.position-ref{position:relative}.content{text-align:center;margin:10px}.title{font-size:36px;padding:20px}img#logo{max-height:150px}
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <img src="/images/logo.svg" alt="Logo" id="logo">
+                <div class="title">
+                  Oops! La pagina richiesta non esiste... 😞
+                </div>
+                Se pensi di essere sulla pagina giusta, prova a ricontrollare<br>
+                il collegamento per errori, ma può darsi che questa pagina non esiste più.
+            </div>
+        </div>
+    </body>
+</html>
